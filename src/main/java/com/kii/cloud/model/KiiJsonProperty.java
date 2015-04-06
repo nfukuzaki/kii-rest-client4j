512da21b474bd433f493b3d125612a7be69a5702
Noriyoshi Fukuzaki <noriyoshi.fukuzaki@kii.com>
2015-04-06 12:43:19 +0900

    implemanted KiiJsonProperty

--- src/main/java/com/kii/cloud/model/KiiJsonProperty.java
+++ src/main/java/com/kii/cloud/model/KiiJsonProperty.java
@@ -1,19 +1,116 @@
 package com.kii.cloud.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.math.BigDecimal;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.kii.cloud.util.GsonUtils;
 
 public class KiiJsonProperty {
 	
-	private final String requestPropertyName;
-	private final String[] responsePropertyName;
+	private final String name;
+	private final String[] aliases;
 	
-	public KiiJsonProperty(String requestPropertyName, String responsePropertyName, String... responsePropertyAlias) {
-		this.requestPropertyName = requestPropertyName;
-		List<String> responsePropertyNames = new ArrayList<String>();
-		responsePropertyNames.add(responsePropertyName);
-		responsePropertyNames.addAll(Arrays.asList(responsePropertyAlias));
-		this.responsePropertyName = responsePropertyNames.toArray(new String[responsePropertyName.length()]);
+	public KiiJsonProperty(String name, String... aliases) {
+		this.name = name;
+		this.aliases = aliases;
+	}
+	public String getString(JsonObject json) {
+		if (json == null) {
+			return null;
+		}
+		if (json.has(this.name)) {
+			return GsonUtils.getString(json, this.name);
+		}
+		for (String alias : this.aliases) {
+			if (json.has(alias)) {
+				return GsonUtils.getString(json, alias);
+			}
+		}
+		return null;
+	}
+	public Boolean getBoolean(JsonObject json) {
+		if (json == null) {
+			return null;
+		}
+		if (json.has(this.name)) {
+			return GsonUtils.getBoolean(json, this.name);
+		}
+		for (String alias : this.aliases) {
+			if (json.has(alias)) {
+				return GsonUtils.getBoolean(json, alias);
+			}
+		}
+		return null;
+	}
+	public Integer getInt(JsonObject json) {
+		if (json == null) {
+			return null;
+		}
+		if (json.has(this.name)) {
+			return GsonUtils.getInt(json, this.name);
+		}
+		for (String alias : this.aliases) {
+			if (json.has(alias)) {
+				return GsonUtils.getInt(json, alias);
+			}
+		}
+		return null;
+	}
+	public Long getLong(JsonObject json) {
+		if (json == null) {
+			return null;
+		}
+		if (json.has(this.name)) {
+			return GsonUtils.getLong(json, this.name);
+		}
+		for (String alias : this.aliases) {
+			if (json.has(alias)) {
+				return GsonUtils.getLong(json, alias);
+			}
+		}
+		return null;
+	}
+	public BigDecimal getBigDecimal(JsonObject json) {
+		if (json == null) {
+			return null;
+		}
+		if (json.has(this.name)) {
+			return GsonUtils.getBigDecimal(json, this.name);
+		}
+		for (String alias : this.aliases) {
+			if (json.has(alias)) {
+				return GsonUtils.getBigDecimal(json, alias);
+			}
+		}
+		return null;
+	}
+	public JsonObject getJsonObject(JsonObject json) {
+		if (json == null) {
+			return null;
+		}
+		if (json.has(this.name)) {
+			return GsonUtils.getJsonObject(json, this.name);
+		}
+		for (String alias : this.aliases) {
+			if (json.has(alias)) {
+				return GsonUtils.getJsonObject(json, alias);
+			}
+		}
+		return null;
+	}
+	public JsonArray getJsonArray(JsonObject json) {
+		if (json == null) {
+			return null;
+		}
+		if (json.has(this.name)) {
+			return GsonUtils.getJsonArray(json, this.name);
+		}
+		for (String alias : this.aliases) {
+			if (json.has(alias)) {
+				return GsonUtils.getJsonArray(json, alias);
+			}
+		}
+		return null;
 	}
 }
