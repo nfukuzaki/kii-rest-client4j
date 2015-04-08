b7f49f6836d178b454cb05a7727f01b073e9665b
Noriyoshi Fukuzaki <noriyoshi.fukuzaki@kii.com>
2015-04-08 14:05:18 +0900

    supports chunked upload

--- src/main/java/com/kii/cloud/resource/KiiObjectBodyResource.java
+++ src/main/java/com/kii/cloud/resource/KiiObjectBodyResource.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import com.google.gson.JsonObject;
 import com.kii.cloud.KiiRestException;
+import com.kii.cloud.model.KiiChunkedUploadContext;
 import com.kii.cloud.resource.KiiRestRequest.Method;
 import com.kii.cloud.util.GsonUtils;
 import com.kii.cloud.util.IOUtils;
@@ -18,7 +19,8 @@ public class KiiObjectBodyResource extends KiiRestSubResource {
 	public static final String BASE_PATH = "/body";
 	
 	public static final MediaType MEDIA_TYPE_START_OBJECT_BODY_UPDATE_REQUEST = MediaType.parse("application/vnd.kii.StartObjectBodyUploadRequest+json");
-	public static final MediaType MEDIA_TYPE_START_OBJECT_BODY_PUBLICATION_REQUEST = MediaType.parse("application/vnd.kii.ObjectBodyPublicationRequest+json");
+	public static final MediaType MEDIA_TYPE_OBJECT_BODY_PUBLICATION_REQUEST = MediaType.parse("application/vnd.kii.ObjectBodyPublicationRequest+json");
+	public static final MediaType MEDIA_TYPE_START_OBJECT_BODY_UPLOAD_REQUEST = MediaType.parse("application/vnd.kii.StartObjectBodyUploadRequest+json");
 	
 	public KiiObjectBodyResource(KiiObjectResource parent) {
 		super(parent);
@@ -82,6 +84,75 @@ public class KiiObjectBodyResource extends KiiRestSubResource {
 		}
 	}
 	/**
+	 * @param contentType
+	 * @param dataSize
+	 * @return
+	 * @throws KiiRestException
+	 * @see http://documentation.kii.com/en/guides/rest/managing-data/object-storages/uploading/
+	 */
+	public KiiChunkedUploadContext beginChunkedUpload(String contentType, long dataSize) throws KiiRestException {
+		Map<String, String> headers = this.newAuthorizedHeaders();
+		KiiRestRequest request = new KiiRestRequest(getUrl("/uploads"), Method.POST, headers, MEDIA_TYPE_START_OBJECT_BODY_UPLOAD_REQUEST, new JsonObject());
+		try {
+			Response response = this.execute(request);
+			JsonObject responseBody = this.parseResponseAsJsonObject(request, response);
+			return new KiiChunkedUploadContext(contentType, GsonUtils.getString(responseBody, "uploadID"), dataSize);
+		} catch (IOException e) {
+			throw new KiiRestException(request.getCurl(), e);
+		}
+	}
+	/**
+	 * @param context
+	 * @param chunk
+	 * @return
+	 * @throws KiiRestException
+	 */
+	public KiiChunkedUploadContext uploadByChunk(KiiChunkedUploadContext context, byte[] chunk) throws KiiRestException {
+		Map<String, String> headers = this.newAuthorizedHeaders();
+		headers.put("Content-Range", "bytes=" + context.getUploadedSize() +
+				"-" + (context.getUploadedSize() + chunk.length - 1) +
+				"/" + context.getDataSize());
+		KiiRestRequest request = new KiiRestRequest(getUrl("/uploads/" + context.getUploadID() + "/data"), Method.PUT, headers, context.getContentType(), chunk);
+		try {
+			Response response = this.execute(request);
+			this.parseResponse(request, response);
+			context.increaseUploadedSize(chunk.length);
+			return context;
+		} catch (IOException e) {
+			throw new KiiRestException(request.getCurl(), e);
+		}
+	}
+	/**
+	 * @param context
+	 * @throws KiiRestException
+	 * @see http://documentation.kii.com/en/guides/rest/managing-data/object-storages/uploading/
+	 */
+	public void commitChunkedUpload(KiiChunkedUploadContext context) throws KiiRestException {
+		Map<String, String> headers = this.newAuthorizedHeaders();
+		KiiRestRequest request = new KiiRestRequest(getUrl("/uploads/" + context.getUploadID() + "/status/committed"), Method.POST, headers);
+		try {
+			Response response = this.execute(request);
+			this.parseResponse(request, response);
+		} catch (IOException e) {
+			throw new KiiRestException(request.getCurl(), e);
+		}
+	}
+	/**
+	 * @param context
+	 * @throws KiiRestException
+	 * @see http://documentation.kii.com/en/guides/rest/managing-data/object-storages/uploading/
+	 */
+	public void cancelChunkedUpload(KiiChunkedUploadContext context) throws KiiRestException {
+		Map<String, String> headers = this.newAuthorizedHeaders();
+		KiiRestRequest request = new KiiRestRequest(getUrl("/uploads/" + context.getUploadID() + "/status/cancelled"), Method.POST, headers);
+		try {
+			Response response = this.execute(request);
+			this.parseResponse(request, response);
+		} catch (IOException e) {
+			throw new KiiRestException(request.getCurl(), e);
+		}
+	}
+	/**
 	 * @return
 	 * @throws KiiRestException
 	 * @see http://documentation.kii.com/en/guides/rest/managing-data/object-storages/publishingbody/
@@ -101,7 +172,7 @@ public class KiiObjectBodyResource extends KiiRestSubResource {
 		if (expiresAt > 0) {
 			requestBody.addProperty("expiresAt", expiresAt);
 		}
-		KiiRestRequest request = new KiiRestRequest(getUrl("/publish"), Method.POST, headers, MEDIA_TYPE_START_OBJECT_BODY_PUBLICATION_REQUEST, requestBody);
+		KiiRestRequest request = new KiiRestRequest(getUrl("/publish"), Method.POST, headers, MEDIA_TYPE_OBJECT_BODY_PUBLICATION_REQUEST, requestBody);
 		try {
 			Response response = this.execute(request);
 			JsonObject responseBody = this.parseResponseAsJsonObject(request, response);
