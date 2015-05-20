5357701871a055ea4cd381d933524b991f71271b
Noriyoshi Fukuzaki <noriyoshi.fukuzaki@kii.com>
2015-05-20 11:34:27 +0900

    improved KiiScope

--- src/main/java/com/kii/cloud/rest/client/model/KiiScope.java
+++ src/main/java/com/kii/cloud/rest/client/model/KiiScope.java
@@ -1,8 +1,22 @@
 package com.kii.cloud.rest.client.model;
 
 public enum KiiScope {
-	APP,
-	GROUP,
-	USER,
-	THING;
+	APP("app", "apps"),
+	GROUP("group", "groups"),
+	USER("user", "users"),
+	THING("thing", "things");
+	
+	private final String name;
+	private final String collectionName;
+	
+	private KiiScope(String name, String collectionName) {
+		this.name = name;
+		this.collectionName = collectionName;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getCollectionName() {
+		return collectionName;
+	}
 }
