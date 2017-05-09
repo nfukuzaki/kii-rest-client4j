0ecaae30399bc01268f60a65a6ebe937467fa064
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2017-05-09 14:20:31 +0900

    added tests

--- src/main/java/com/kii/cloud/rest/client/model/servercode/KiiServerCodeVersion.java
+++ src/main/java/com/kii/cloud/rest/client/model/servercode/KiiServerCodeVersion.java
@@ -7,6 +7,7 @@ import com.kii.cloud.rest.client.model.KiiJsonProperty;
 public class KiiServerCodeVersion extends KiiJsonModel {
 	
 	public static final KiiJsonProperty<String> PROPERTY_VERSION_ID = new KiiJsonProperty<String>(String.class, "versionID");
+	public static final KiiJsonProperty<String> PROPERTY_ENVIRONMENT_VERSION = new KiiJsonProperty<String>(String.class, "environmentVersion");
 	public static final KiiJsonProperty<Long> PROPERTY_CREATED_AT = new KiiJsonProperty<Long>(Long.class, "createdAt");
 	public static final KiiJsonProperty<Long> PROPERTY_MODIFIED_AT = new KiiJsonProperty<Long>(Long.class, "modifiedAt");
 	public static final KiiJsonProperty<Boolean> PROPERTY_CURRENT = new KiiJsonProperty<Boolean>(Boolean.class, "current");
@@ -17,6 +18,9 @@ public class KiiServerCodeVersion extends KiiJsonModel {
 	
 	public String getVersionID() {
 		return PROPERTY_VERSION_ID.get(this.json);
+	}
+	public String getEnvironmentVersion() {
+		return PROPERTY_ENVIRONMENT_VERSION.get(this.json);
 	}
 	public Long getCreatedAt() {
 		return PROPERTY_CREATED_AT.get(this.json);
